d4c7bf975d8adf43457efa3bf60f4582b2a177f3
arkad <arkadiuszsz@gmail.com>
2021-01-27 23:43:30 +0100

    Initial commit

---
+++ src/main/java/com/vaadin/tutorial/crm/backend/repository/ContactRepository.java
@@ -0,0 +1,12 @@
+package com.vaadin.tutorial.crm.backend.repository;
+
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+}
